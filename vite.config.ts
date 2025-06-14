683abfec0d66ff4a186f94a5bdd0b7638e14f3ef
Oleksandr Shamalo <shamalo1989@gmail.com>
2025-06-14 17:23:52 +0300

    Added 404.html file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-react-project/',
+  base: '/Homework-38/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
